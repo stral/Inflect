2f417d87e17dbef53f3f2a762128b74830e584c3
Randson Oliveira <randsonjs@gmail.com>
2014-09-10 10:01:17 -0400

    Add classes for test phpunit

---
+++ src/Application/NativeElements/Math.php
@@ -0,0 +1,16 @@
+<?php 
+
+namespace Application\NativeElements;
+
+class Math {
+
+	/**
+	 * Sum parameters
+	 *
+	 * @return void
+	 **/
+	public function sum($value1, $value2)
+	{
+		return $value1 + $value2;
+	}
+}
--- tests/PHPNativeElementsTest.php
+++ tests/PHPNativeElementsTest.php
@@ -11,7 +11,7 @@ class PHPNativeElementsTest extends PHPUnit {
 	 **/
 	public function setUp()
 	{
-
+		$this->math = new Math;
 	}
 
 	/**
@@ -21,7 +21,17 @@ class PHPNativeElementsTest extends PHPUnit {
 	 **/
 	public function tearDown()
 	{
-		
+
+	}
+
+	/**
+	 * Test Operation Math
+	 *
+	 * @return void
+	 **/
+	public function testMath()
+	{
+		$this->assertEquals(3, $this->math->(1, 2), 'Não somou corretamente');
 	}
 
 }
