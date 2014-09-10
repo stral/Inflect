bdbad67382bb0b6d433afb33a5bb7897c196ae9c
Randson Oliveira <randsonjs@gmail.com>
2014-09-10 09:08:44 -0400

    Adicionado classe para tests no phpunit

---
+++ tests/PHPNativeElementsTest.php
@@ -0,0 +1,26 @@
+<?php 
+
+use PHPUnit_Framework_TestCase as PHPUnit;
+
+class PHPNativeElementsTest extends PHPUnit {
+
+	/**
+	 * undocumented function
+	 *
+	 * @return void
+	 **/
+	public function setUp()
+	{
+
+	}
+
+	/**
+	 * undocumented function
+	 *
+	 * @return void
+	 **/
+	public function tearDown()
+	{
+	}
+
+}
