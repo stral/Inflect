d5a440e3646702567ad29b1b780191584c44f7b4
Randson Oliveira <randsonjs@gmail.com>
2014-09-10 09:19:45 -0400

    Nova classe de test

---
+++ tests/PHPInflectTest.php
@@ -0,0 +1,28 @@
+<?php 
+
+use PHPUnit_Framework_TestCase as PHPUnit;
+use Application\Inflect\Inflect as Inflect;
+
+class InflectTest extends PHPUnit {
+
+	/**
+	 * undocumented function
+	 *
+	 * @return void
+	 **/
+	public function setUp()
+	{
+
+	}
+
+	/**
+	 * undocumented function
+	 *
+	 * @return void
+	 **/
+	public function tearDown()
+	{
+		
+	}
+	
+}
